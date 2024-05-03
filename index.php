919ff11a9128447060fd937c125ea1d543533cf9
SilvioGiacomelli <91.giacomelli.silvio@gmail.com>
2024-05-03 15:05:55 +0200

    [INIZIO]

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once __DIR__ . '/data/db.php';
+require_once __DIR__ . '/Model/ecommerce.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
